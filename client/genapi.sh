139d3f2b79c939300c45e33807272b7b535a3afc
Max <hess@blacklight-it.com>
2015-10-27 09:50:54 +0100

    removed obsolete c++ client code

--- client/genapi.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-mkdir -p api
-jsonrpcstub spec.json --cpp-client=ClientHandler
-mv clienthandler.h api/clienthandler.hpp
--- client/src/user.cpp
+++
@@ -1,9 +0,0 @@
-#include "../include/user.hpp"
-#include "../include/cryptohelper.hpp"
-
-User::User(std::string username, std::shared_ptr<CryptoHelper> cryptoHelper)
-		: m_username(username), m_cryptoHelper(cryptoHelper), m_userKeyPair(m_cryptoHelper->generateKeyPair()),
-			m_exchangeKeyPair(m_cryptoHelper->generateKeyPair()) {
-
-}
-
