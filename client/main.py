dddcd0351d32bcea599b43edb12b6cab902831be
Max <mail@ne0h.de>
2015-10-27 23:01:42 +0100

    client: sodium integration test

---
+++ client/main.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python2
+
+import sys
+sys.path.append("../3rdparty/pysodium-build/lib/python2.7/site-packages/pysodium-0.6.7-py2.7.egg")
+print sys.path
+import pysodium
+#keypair = 
