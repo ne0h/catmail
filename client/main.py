6e9ca161ba33bd23858902d9286e88374c7b23ca
Max <mail@ne0h.de>
2015-10-27 23:25:01 +0100

    added correct pysodium package and generated first keypair

--- client/main.py
+++ client/main.py
@@ -2,6 +2,7 @@
 
 import sys
 sys.path.append("../3rdparty/pysodium-build/lib/python2.7/site-packages/pysodium-0.6.7-py2.7.egg")
-print sys.path
 import pysodium
-#keypair = 
+
+pk, sk = pysodium.crypto_box_keypair()
+print pk
