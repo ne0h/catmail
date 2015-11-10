7f48c7b09bbf3668b15acf8d5ccec1d3d106ad00
Max <mail@ne0h.de>
2015-11-10 10:13:47 +0100

    client: corrected python include paths

--- client/main.py
+++ client/main.py
@@ -1,10 +1,10 @@
 import sys
 curVersion = str(sys.version_info.major) + "." + str(sys.version_info.minor)
-sys.path.append("/Users/ne0h/Projekte/catmail/3rdparty/thrift-build/lib/python" + curVersion
+sys.path.append("../3rdparty/thrift-build/lib/python" + curVersion
 	+ "/site-packages/thrift.egg")
-sys.path.append("/Users/ne0h/Projekte/catmail/3rdparty/thrift-build/lib/python" + curVersion
+sys.path.append("../3rdparty/thrift-build/lib/python" + curVersion
 	+ "/site-packages/six.egg")
-sys.path.append("/Users/ne0h/Projekte/catmail/client/api/protocol")
+sys.path.append("./api/protocol")
 sys.path.append("../3rdparty/pysodium-build/lib/python" + curVersion + "/site-packages/pysodium-0.6.7-py" 
 	+ curVersion + ".egg")
 
