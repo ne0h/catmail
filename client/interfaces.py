93d297c99d10f679da5b9909a957641ed423fb7b
Florian Muenchbach <florian.muenchbach@student.kit.edu>
2015-11-23 12:06:38 +0100

    [client] added description of internal interfaces / abstract classes.

---
+++ client/interfaces.py
@@ -0,0 +1,13 @@
+class ClientBackend:
+    def __init__(self):
+        pass
+
+class ClientInterface:
+    def first_run(self):
+        raise NotImplementedError()
+
+    def show(self):
+        raise NotImplementedError()
+
+    def __init__(self):
+        pass
