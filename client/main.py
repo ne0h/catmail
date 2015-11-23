d364e051fbb0476dafffebc4dde71df14c198c41
Florian Muenchbach <florian.muenchbach@student.kit.edu>
2015-11-23 12:00:50 +0100

    [client] Switched to argparse for cmd argument parsing.

--- client/main.py
+++ client/main.py
@@ -1,4 +1,5 @@
 import sys
+import argparse
 curVersion = str(sys.version_info.major) + "." + str(sys.version_info.minor)
 sys.path.append("../3rdparty/thrift-build/lib/python" + curVersion
 	+ "/site-packages/thrift.egg")
@@ -9,30 +10,48 @@ sys.path.append("../3rdparty/pysodium-build/lib/python" + curVersion + "/site-pa
 	+ curVersion + ".egg")
 
 from catmailclient import CatMailClient
+from catmailclientbackend import CatMailClientBackend
 import cryptohelper
  
-if __name__ == '__main__':
-
-	if len(sys.argv) == 1:
-		CatMailClient()
-	else:
-		cmd = sys.argv[1]
-
-		if (cmd == "help"):
-			print("Usage:")
-			print("python main.py help                         		Prints this information")
-			print("python main.py new username password        		Generates all crypto data for a new client")
-			print("python main.py testlogin username password       Does a test login")
-
-		# Generates all crypto data for a new client and prints them.
-		# This data is to be transfered to catmail server manually.
-		elif cmd == "new":
-			cryptohelper.newClient(sys.argv[2], sys.argv[3])
-
-		# Takes command line arguments and does a test login.
-		elif cmd == "testlogin":
-			CatMailClient(nogui=True).login(sys.argv[2], sys.argv[3], testlogin=True)
-
-		else:
-			print("Command not known.")
+def setup_argparse():
+	newline = '\n'
+	parser = argparse.ArgumentParser(description="CatMail Client Application")
 	
+	parser.add_argument("--new",
+			help="Generates all crypto data for a new client",
+			nargs=2,
+			type=str
+		)
+	parser.add_argument("--testlogin",
+			help="Does a test login",
+			nargs=2,
+			type=str
+		)
+	return parser
+
+def __start(nogui=False):
+	gui = None
+	if not nogui:
+		gui = CatMailClient()
+	client = CatMailClientBackend(frontend=gui)
+	client.start(nogui=nogui)
+
+# Takes command line arguments and does a test login.
+def __testlogin(username, password):
+	client = CatMailClientBackend()
+	client.login(username, password, testlogin=True)
+
+# Generates all crypto data for a new client and prints them.
+# This data is to be transfered to catmail server manually.
+def __start_fresh(username, password):
+	cryptohelper.newClient(sys.argv[2], sys.argv[3])
+
+if __name__ == '__main__':
+	args = setup_argparse().parse_args()
+
+	if (args.testlogin):
+		__testlogin(*args.testlogin)
+	elif (args.new):
+		__start_fresh(*args.testlogin)
+	else:
+	        __start()
