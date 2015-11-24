83a4e6362b8603a3cb7ad056fc4664eeecb927ca
Max <mail@ne0h.de>
2015-11-24 10:47:17 +0100

    [server] added connection pool for parallel connections and auto reconnect

--- server/databasehandler.js
+++ server/databasehandler.js
@@ -8,132 +8,157 @@ function DatabaseHandler(settings) {
 
 	var that = this;
 
-	this.conn = mysql.createConnection({
+	var pool = mysql.createPool({
 		host:     settings.hostname,
 		user:     settings.username,
 		password: settings.password,
 		database: settings.database
 	});
 
-	this.conn.connect(function(err) {
-		if (err) {
-			Logger.error("Failed to connect to database server:" + err.stack);
-			process.exit();
-		} else {
-			Logger.debug("Successfully connected to database server");
-		}
-	});
-
 	this.validatePasswordLogin = function(username, password, callback) {
-		var sql = "SELECT EXISTS (SELECT `username`, `password` FROM users"
-			+ " WHERE `username`=? AND `password`=?) AS result;";
-		this.conn.query(sql, [username, password], function(err, result) {
+		
+		var sql = "SELECT EXISTS (SELECT `username`, `password` FROM users WHERE `username`=? AND `password`=?)"
+			+ " AS result;";
+		
+		pool.getConnection(function(err, conn) {
 			if (err) {
-				Logger.error("Failed to validate password for " + username + ": " + err.stack);
-				callback(err, null);
+				Logger.error("Failed to get mysql connection form pool: " + err.stack);
+				callback(new CatMailTypes.InternalException(), null);
 				return;
-			} else {
-				callback(null, result[0].result)}
+			}
+
+			conn.query(sql, [username, password], function(err, result) {
+				conn.release();
+				if (err) {
+					Logger.error("Failed to validate password for " + username + ": " + err.stack);
+					callback(new CatMailTypes.InternalException(), null);
+					return;
+				} else {
+					callback(null, result[0].result)}
+			});
 		});
 	}
 
 	this.getPrivateKeys = function(username, callback) {
-		var sql = "SELECT `userkeypair_sk`, `userkeypair_pk`,"
-			+ " `userkeypair_nonce`, `exchangekeypair_sk`,"
-			+ " `exchangekeypair_pk`, `exchangekeypair_nonce` FROM `users`"
-			+ " WHERE `username`=?;";
-		this.conn.query(sql, [username], function(err, result) {
+
+		var sql = "SELECT `userkeypair_sk`, `userkeypair_pk`, `userkeypair_nonce`, `exchangekeypair_sk`,"
+			+ " `exchangekeypair_pk`, `exchangekeypair_nonce` FROM `users` WHERE `username`=?;";
+
+		pool.getConnection(function(err, conn) {
 			if (err) {
-				Logger.error("Failed to get private keys for " + username + ": " + err.stack);
-				callback(err, null);
+				Logger.error("Failed to get mysql connection form pool: " + err.stack);
+				callback(new CatMailTypes.InternalException(), null);
 				return;
 			}
 
-			var userKeyPair = new CatMailTypes.EncryptedKeyPair();
-			userKeyPair.encryptedSecretKey = result[0].userkeypair_sk;
-			userKeyPair.nonce = result[0].userkeypair_nonce;
-			userKeyPair.publicKey = result[0].userkeypair_pk;
+			conn.query(sql, [username], function(err, result) {
+				conn.release();
+				if (err) {
+					Logger.error("Failed to get private keys for " + username + ": " + err.stack);
+					callback(new CatMailTypes.InternalException(), null);
+					return;
+				}
 
-			var exchangeKeyPair = new CatMailTypes.EncryptedKeyPair();
-			exchangeKeyPair.encryptedSecretKey = result[0].exchangekeypair_sk;
-			exchangeKeyPair.nonce = result[0].exchangekeypair_nonce;
-			exchangeKeyPair.publicKey = result[0].exchangekeypair_pk;
+				var userKeyPair = new CatMailTypes.EncryptedKeyPair();
+				userKeyPair.encryptedSecretKey = result[0].userkeypair_sk;
+				userKeyPair.nonce = result[0].userkeypair_nonce;
+				userKeyPair.publicKey = result[0].userkeypair_pk;
 
-			var response = new CatMailTypes.GetPrivateKeysResponse();
-			response.userKeyPair = userKeyPair;
-			response.exchangeKeyPair = exchangeKeyPair;
+				var exchangeKeyPair = new CatMailTypes.EncryptedKeyPair();
+				exchangeKeyPair.encryptedSecretKey = result[0].exchangekeypair_sk;
+				exchangeKeyPair.nonce = result[0].exchangekeypair_nonce;
+				exchangeKeyPair.publicKey = result[0].exchangekeypair_pk;
 
-			callback(null, response);
+				var response = new CatMailTypes.GetPrivateKeysResponse();
+				response.userKeyPair = userKeyPair;
+				response.exchangeKeyPair = exchangeKeyPair;
+
+				callback(null, response);
+			});
 		});
 	}
 
 	this.getExchangeKeyPairPublicKey = function(username, callback) {
-		var sql = "SELECT `exchangekeypair_pk` FROM `users` "
-			+ "WHERE `username`=?;";
-		this.conn.query(sql, [username], function(err, result) {
+
+		var sql = "SELECT `exchangekeypair_pk` FROM `users` WHERE `username`=?;";
+
+		pool.getConnection(function(err, conn) {
 			if (err) {
-				Logger.error("Failed to get exchange keypair for " + username + ": " + err.stack);
-				callback(err, null);
+				Logger.error("Failed to get mysql connection form pool: " + err.stack);
+				callback(new CatMailTypes.InternalException(), null);
 				return;
 			}
 
-			callback(null, result[0].exchangekeypair_pk);
+			conn.query(sql, [username], function(err, result) {
+				conn.release();
+				if (err) {
+					Logger.error("Failed to get exchange keypair for " + username + ": " + err.stack);
+					callback(err, null);
+					return;
+				}
+
+				callback(null, result[0].exchangekeypair_pk);
+			});
 		});
 	}
 
 	this.getContactList = function(username, version, callback) {
-		var sql = "SELECT `contactname` FROM `contacts` WHERE `username`=?"
-			+ " AND version>?;";
-		this.conn.query(sql, [username, version], function(err, result) {
+
+		var sql = "SELECT `contactname` FROM `contacts` WHERE `username`=? AND version>?;";
+
+		pool.getConnection(function(err, conn) {
 			if (err) {
-				Logger.error("Failed to get contactlist for " + username + ": " + err.stack);
-				callback(err, null);
+				Logger.error("Failed to get mysql connection form pool: " + err.stack);
+				callback(new CatMailTypes.InternalException(), null);
 				return;
 			}
 
-			var response = new CatMailTypes.GetContactListResponse();
-			response.contacts = [];
+			conn.query(sql, [username, version], function(err, result) {
+				conn.release();
+				if (err) {
+					Logger.error("Failed to get contactlist for " + username + ": " + err.stack);
+					callback(new CatMailTypes.InternalException(), null);
+					return;
+				}
 
-			for (var i in result) {response.contacts.push(new CatMailTypes.Contact(result[i].contactname), {});}
+				var response = new CatMailTypes.GetContactListResponse();
+				response.contacts = [];
 
-			callback(null, response);
+				for (var i in result) {response.contacts.push(new CatMailTypes.Contact(result[i].contactname), {});}
+
+				callback(null, response);
+			});
 		});
 	}
 
 	this.addToContactList = function(username, userToAdd, attributes, callback) {
-		this.conn.beginTransaction(function(err) {
+			
+		pool.getConnection(function(err, conn) {
 			if (err) {
-				Logger.error("Failed to add " + userToAdd + " to contact list of " + username + " " + err.stack)
-				callback(err, null);
+				Logger.error("Failed to get mysql connection form pool: " + err.stack);
+				callback(new CatMailTypes.InternalException(), null);
 				return;
 			}
-			
-			var sql = "UPDATE `users` SET `contacts_version`=`contacts_version`+1 WHERE `username`=?;";
-			that.conn.query(sql, [username], function(err, result) {
-				if (err) {
-					return that.conn.rollback(function() {
-						Logger.error("Failed to add " + userToAdd + " to contact list of " + username + " " + err.stack)
-						callback(err, null);
-						return;
-					});
-				}
 
-			sql = "SELECT `contacts_version` FROM `users` WHERE `username`=?;";
-			that.conn.query(sql, [username], function(err, result) {
+			conn.beginTransaction(function(err) {
+				if (err) {
+					Logger.error("Failed to add " + userToAdd + " to contact list of " + username + " " + err.stack)
+					callback(new CatMailTypes.InternalException(), null);
+					return;
+				}
+				
+				var sql = "UPDATE `users` SET `contacts_version`=`contacts_version`+1 WHERE `username`=?;";
+				conn.query(sql, [username], function(err, result) {
 					if (err) {
 						return that.conn.rollback(function() {
-							Logger.error("Failed to add " + userToAdd + " to contact list of " + username + " "
-								+ err.stack)
+							Logger.error("Failed to add " + userToAdd + " to contact list of " + username + " " + err.stack)
 							callback(err, null);
 							return;
 						});
 					}
-			
-					var version = result[0].contacts_version;
 
-					sql = "INSERT INTO `contacts` SET ?;";
-					that.conn.query(sql, [{"username": username, "contactname": userToAdd,
-							"version": version}], function(err, result) {
+				sql = "SELECT `contacts_version` FROM `users` WHERE `username`=?;";
+				conn.query(sql, [username], function(err, result) {
 						if (err) {
 							return that.conn.rollback(function() {
 								Logger.error("Failed to add " + userToAdd + " to contact list of " + username + " "
@@ -142,8 +167,12 @@ function DatabaseHandler(settings) {
 								return;
 							});
 						}
+				
+						var version = result[0].contacts_version;
 
-						that.conn.commit(function(err) {
+						sql = "INSERT INTO `contacts` SET ?;";
+						conn.query(sql, [{"username": username, "contactname": userToAdd,
+								"version": version}], function(err, result) {
 							if (err) {
 								return that.conn.rollback(function() {
 									Logger.error("Failed to add " + userToAdd + " to contact list of " + username + " "
@@ -153,12 +182,24 @@ function DatabaseHandler(settings) {
 								});
 							}
 
-							var response = new CatMailTypes.AddToContactListResponse();
-							response.version = version;
+							conn.commit(function(err) {
+								conn.release();
+								if (err) {
+									return that.conn.rollback(function() {
+										Logger.error("Failed to add " + userToAdd + " to contact list of " + username + " "
+											+ err.stack)
+										callback(err, null);
+										return;
+									});
+								}
 
-							callback(null, response);
-						});
-					})
+								var response = new CatMailTypes.AddToContactListResponse();
+								response.version = version;
+
+								callback(null, response);
+							});
+						})
+					});
 				});
 			});
 		});
