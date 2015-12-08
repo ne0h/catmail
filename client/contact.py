4a9a5995d0813e5df8d2623b34c6dac290ac7ba2
Florian Muenchbach <florian.muenchbach@gmail.com>
2015-12-08 14:36:25 +0100

    [client] added basic contact class.

---
+++ client/contact.py
@@ -0,0 +1,21 @@
+class CatMailContact:
+    def has_avatar(self):
+        #TODO not yet implemented
+        return False
+
+    def getContactID(self):
+        return self.cid
+
+    def getAlias(self):
+        return self.alias if not self.alias is None else self.cid
+
+    def equals(self, contact):
+        return (
+                self.getContactID() == contact.getContactID()
+                and self.getAlias() == contact.getAlias()
+            )
+
+    def __init__(self, cid, alias=None):
+        print(cid)
+        self.cid = cid
+        self.alias = alias
