3e71431f04ae2da472c2bf36d3daa9ba9d8e47c6
Max <mail@ne0h.de>
2016-03-23 20:48:42 +0100

    [server] updated structure of testcases

--- server/tests/testusercreationanddeletion.py
+++ server/tests/testusercreationanddeletion.py
@@ -1,37 +1,44 @@
-import random, string
+import logging, random, string, unittest
 from testclient import TestClient
 from constants import ErrorCodes
 import cryptohelper
 
-class TestUserCreationAndDeletion(TestClient):
+class TestUserCreationAndDeletion(unittest.TestCase):
 
-	def __init__(self):
-		super().__init__()
-		
+	def test_userCreationAndDeletion(self):
+		testClient = TestClient()
+
 		# create random user data
 		username = "".join(random.SystemRandom().choice(string.ascii_lowercase) for _ in range(16)) + "@catmail.de"
 		password = "".join(random.SystemRandom().choice(string.ascii_lowercase) for _ in range(16))
 		
-		# create user
-		print("Trying to create new user called %s" % username)
-		(err, result) = self._client.createUser(username, password)
-		if err is ErrorCodes.NoError:
-			print("Created %s" % username)
-		else:
-			print("Creation failed: %s" % err)
-		
-		print("Trying to create user with same username")
-		(err, result) = self._client.createUser(username, password)
-		if err is ErrorCodes.NoError:
-			print("Created %s" % username)
-		else:
-			print("Creation failed: %s" % err)
+		#
+		# CREATION STUFF
+		#
 
-		(err, result) = self._client.deleteUser(username, self._sessionToken)
-		if err is ErrorCodes.NoError:
-			print("Deleted %s" % username)
-		else:
-			print("Deletion failed: %s" % err)
+		# create user
+		(err, result) = testClient._client.createUser(username, password)
+		self.assertIsInstance(ErrorCodes.NoError, type(err))
+		if err is not ErrorCodes.NoError:
+			print("Trying to create new user called %s. Creation failed: %s" % (username, err))
+		
+		# trying to create same user again
+		(err, result) = testClient._client.createUser(username, password)
+		self.assertIsInstance(ErrorCodes.UserAlreadyExists, type(err))
+		if err is not ErrorCodes.UserAlreadyExists:
+			print("Trying to create user with same username. Creation failed: %s" % err)
+
+		#
+		# DELETION STUFF
+		#
+
+		# delete user
+		(err, result) = testClient._client.deleteUser(username, testClient._sessionToken)
+		self.assertIsInstance(ErrorCodes.InvalidSession, type(err))
+		if err is not ErrorCodes.InvalidSession:
+			print("Deleted %s. Deletion failed: %s" % (username, err))
+
+		# log new user in to delete itsself
 
 if __name__ == "__main__":
-	TestUserCreationAndDeletion()
+	unittest.main()
