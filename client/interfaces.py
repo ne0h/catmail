64fc4f96f1571d30af0a280ebc25794004527b55
Florian Muenchbach <florian.muenchbach@gmail.com>
2016-06-07 14:16:35 +0200

    [client] added create_conversation to interfaces.

--- client/interfaces.py
+++ client/interfaces.py
@@ -2,6 +2,9 @@ class ClientBackend:
     def update_contacts(self, callback=None):
         raise NotImplementedError()
 
+    def create_conversation(self, contact_ids, callback=None):
+        raise NotImplementedError()
+
     def __init__(self):
         pass
 
