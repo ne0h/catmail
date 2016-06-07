dbdf3d79bb0caf2153e8e5fa0f8898cf756998ac
Florian Muenchbach <florian.muenchbach@gmail.com>
2016-06-07 14:17:44 +0200

    [client] added slots (mostly stubs).

--- client/catmailclient.py
+++ client/catmailclient.py
@@ -15,7 +15,7 @@ from contact import CatMailContact
 
 # TODO make sure there is only one window open at a time
 
-class CatMailClient(ClientInterface):
+class CatMailClient(QObject, ClientInterface):
 	def show_dialog(self, title, message, buttons):
 		dialog = MessageDialog()
 		rv = dialog.show_blocking(
@@ -80,15 +80,80 @@ class CatMailClient(ClientInterface):
 	def __on_contacts_updated(self, contacts):
 		self.contact_list = contacts
 		self.main_window.set_contact_list(contacts)
-		self.main_window.update_contacts()
+		self.main_window.update_contact_list()
+
+	def __on_conversations_updated(self):
+		pass
 
 	def update_conversations(self, conversations):
 		self.conversations_list = conversations
 
+	""" Listener that is triggered when the backend has created a new chat.
+
+		@note This differs from __on_conversations_updated in that it will be
+		called as a response to a backend.create_conversation call.
+	"""
+	def __on_conversation_created(self, chatid):
+		pass
+
 	def __on_send_message(self, message, conversationID):
 	    print("conversation: %s, message: '%s'" %
 				(conversationID, message))
-		
+
+	def __on_open_private_chat_intent(self, contactID):
+		print("received chat open intent for: %s" % contactID)
+		conversations_with_cid \
+				= self.conversations_list.get_conversations_with_contact_id(
+					self,
+					cid,
+					private_only=True
+				)
+		if len(conversations_with_cid) == 0:
+			# Asynchronous call to the backend.
+			# This will trigger __on_chat_created
+			self.backend.create_conversation(
+					[contactID],
+					self.__on_conversation_created
+				)
+		else:
+			# select conversation
+			print("conversations found: %s" % conversations_with_cid)
+			pass
+
+	def __update_conversations(self):
+		#TODO
+		pass
+
+	def __on_contact_added(self, error, cid):
+		if error == ErrorCodes.NoError:
+			self.__update_contacts()
+		else:
+			message = "Failed to add contact '%s'." % cid
+			self.show_error(message)
+			self.logger.error("%s: %s" % (message, error))
+
+	def __on_add_contact_intent(self, cid):
+		self.backend.addToContactList(cid, self.__on_contact_added)
+		pass
+
+	def __on_add_conversation_intent(self, conversation):
+		#TODO
+		pass
+
+	def __update_contacts(self):
+		print("__update_contacts")
+		self.backend.update_contacts(self.__on_contacts_updated)
+
+	def __connect_mani_window_signals(self):
+		self.main_window.send_message.connect(self.__on_send_message)
+		self.main_window.open_chat_intent.connect(
+				self.__on_open_private_chat_intent
+			)
+		self.main_window.update_contacts_intent.connect(self.__update_contacts)
+		self.main_window.update_conversations_intent.connect(self.__update_conversations)
+		self.main_window.add_contact_intent.connect(self.__on_add_contact_intent)
+		self.main_window.add_conversation_intent.connect(self.__on_add_conversation_intent)
+
 	def show(self):
 		#TODO...
 		# check if there is a session token
@@ -100,8 +165,8 @@ class CatMailClient(ClientInterface):
 
 		#self.app = QApplication(sys.argv)
 		self.main_window = MainWindow(self, self.app)
-		self.backend.update_contacts(self.__on_contacts_updated)
-		# self.__connect_mani_window_signals()
+		self.__update_contacts()
+		self.__connect_mani_window_signals()
 		self.main_window.show()
 
 	def add_conversation(self, conversationId, title=None):
@@ -114,7 +179,7 @@ class CatMailClient(ClientInterface):
 	#TODO remove when threaded, debug only!
 	def wait(self):
 		sys.exit(self.app.exec_())
-		
+
 	def addToContactList(self, username):
 		print("self.__serverHandler.")
 
